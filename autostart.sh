90333b077f9adf7f4b3a5c1c87f637db377ea9bf
Chet Corcos <ccorcos@gmail.com>
2019-02-02 23:31:01 -0800

    print reboot

--- autostart.sh
+++ autostart.sh
@@ -4,4 +4,4 @@ sudo cp recorder.service /lib/systemd/system/recorder.service
 sudo systemctl daemon-reload
 sudo systemctl enable recorder.service
 
-echo "Reboot with `sudo reboot`."
+echo "Reboot with 'sudo reboot'."
